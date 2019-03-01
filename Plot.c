75486c24e158a8786600573fdcbfcbdec3660bfd
PatriktheAstronomer <37809773+PatriktheAstronomer@users.noreply.github.com>
2019-03-01 21:21:24 +0100

    Add files via upload

--- Plot.c
+++ Plot.c
@@ -10,33 +10,34 @@ void Plot() {
    c1->SetGrid();
    c1->SetFillColor(kWhite);
 
-   const Int_t n = 2206; //maximum number of lines, starts from second line!
+   const Int_t n = 36; //maximum number of lines, starts from second line!
    Double_t x1[n], y1[n];
    for(Int_t i = 0; i < n; i++){ //instead of using EOF
 	x1[i] = 0;
         y1[i] = 0;
    }
    FILE* data;
-   data = fopen("Data_termoclanek_Novotny.dat","r"); //source name
-   fscanf(data, "%lf  %lf", &x1[0], &y1[0]);
+   data = fopen("Odporvoda.txt","r"); //source name
+   fscanf(data, "%lf	%lf", &x1[0], &y1[0]);
    for(Int_t i = 1; i < n; i++){
-   	fscanf(data, "%lf  %lf", &x1[i], &y1[i]);
+   	fscanf(data, "%lf	%lf", &x1[i], &y1[i]);
 	}
    fclose(data);
 /*   for(Int_t i = 0; i < n; i++){
 	printf("%lf  %lf\n",x1[i],y1[i]);
    } diagnostic loop */
    TGraph *gr1 = new TGraph(n,x1,y1);
-   gr1->SetTitle("Thermoelectrical voltage - calibration");
-   gr1->GetXaxis()->SetTitle("Time [s]");
-   gr1->GetYaxis()->SetTitle("Thermoelectric Voltage [V]");
+   gr1->SetTitle("");
+   gr1->GetXaxis()->SetTitle("#tau [s]");//using latex features
+   gr1->GetYaxis()->SetTitle("R [Ohm]");
    gr1->GetXaxis()->CenterTitle(1);
    gr1->GetYaxis()->CenterTitle(1);
    gr1->SetFillColor(kWhite);
    gr1->SetLineColor(2);
    gr1->SetLineWidth(1);
-   gr1->Draw("AC"); //using ("AC*" use * as a marker)
-	
+   gr1->Draw("AC*"); //using ("AC*" use * as a marker)
+   
+   
    c1->Update();
    c1->GetFrame()->SetFillColor(0);
    c1->GetFrame()->SetBorderSize(12);
